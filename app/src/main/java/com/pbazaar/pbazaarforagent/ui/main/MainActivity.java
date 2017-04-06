c9dc49ad1aa78d2d6149fc6cdbfed1bd8573a9fa
supto09 <supto09apee@gmail.com>
2017-04-06 23:44:08 +0600

    permission request for marshmallow and later added

--- app/src/main/java/com/pbazaar/pbazaarforagent/ui/main/MainActivity.java
+++ app/src/main/java/com/pbazaar/pbazaarforagent/ui/main/MainActivity.java
@@ -1,7 +1,14 @@
 package com.pbazaar.pbazaarforagent.ui.main;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -20,12 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final int MY_PERMISSIONS_REQUEST_CODE = 420;
+    public static final String ACTION = "no_permission_receiver";
+
     @BindView(R.id.fragment_container_main_activity)
     FrameLayout fragmentContainer;
 
 
     private ProductPostingFragment productPostingFragment;
 
+    private final BroadcastReceiver mNotificationReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "ON RECEIVE");
+
+            NoPermissionDialog noPermissionDialog = new NoPermissionDialog();
+            noPermissionDialog.setOnConfirmButtonmClickListener(new NoPermissionDialog.OnConfirmButtonmClickListener() {
+                @Override
+                public void onConfirmButtonClicked() {
+
+                    requestPermission();
+                }
+            });
+            noPermissionDialog.show(MainActivity.this.getSupportFragmentManager(), NoPermissionDialog.class.getSimpleName());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +73,22 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(fragmentContainer.getId(), productPostingFragment, "AAAA").commit();
 
+
+        // check for the permission
+        LocalBroadcastManager.getInstance(this).registerReceiver(mNotificationReceiver, new IntentFilter(ACTION));
+        requestPermission();
+
+
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mNotificationReceiver);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -68,6 +109,61 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CODE: {
+
+                boolean status = true;
+                Log.d(TAG, "statussize : " + grantResults.length);
+                for (int reult : grantResults) {
+
+                    Log.d(TAG, "status: " + reult);
+
+                    if (reult != PackageManager.PERMISSION_GRANTED) {
+                        status = false;
+                        break;
+                    }
+                }
+
+                if (!status) {
+
+                    // permission was granted, yay! do the
+                    // calendar task you need to do.
+
+                    Intent intent = new Intent(ACTION);
+                    LocalBroadcastManager.getInstance(MainActivity.this).sendBroadcast(intent);
+
+                }
+                return;
+            }
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE
+                                , Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                        , MY_PERMISSIONS_REQUEST_CODE);
+                return;
+            }
+        }
+    }
+
+
     private void logout() {
         // clear the customerId saved in preference and start login activity
 
@@ -78,12 +174,4 @@ public class MainActivity extends AppCompatActivity {
         startActivity(logInActivityIntent);
         overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-
-        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
-    }
-
 }
---
+++ app/src/main/java/com/pbazaar/pbazaarforagent/ui/main/NoPermissionDialog.java
@@ -0,0 +1,61 @@
+package com.pbazaar.pbazaarforagent.ui.main;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+
+import com.pbazaar.pbazaarforagent.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Pathshala on 04-Mar-17.
+ */
+
+public class NoPermissionDialog extends DialogFragment {
+
+    private TextView okButtonText;
+
+    private OnConfirmButtonmClickListener onConfirmButtonmClickListener;
+
+    public NoPermissionDialog() {
+    }
+
+    public void setOnConfirmButtonmClickListener(OnConfirmButtonmClickListener onConfirmButtonmClickListener) {
+        this.onConfirmButtonmClickListener = onConfirmButtonmClickListener;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View view = inflater.inflate(R.layout.fragment_permission_required, null);
+
+        okButtonText = (TextView) view.findViewById(R.id.no_permission_description_button);
+
+        okButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+                if (onConfirmButtonmClickListener != null) {
+                    onConfirmButtonmClickListener.onConfirmButtonClicked();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .create();
+        return alertDialog;
+    }
+
+    public interface OnConfirmButtonmClickListener {
+        void onConfirmButtonClicked();
+    }
+}
